5ffa6510a3a6685caeeaf60c99a7606a1b691da7
ngocquang95 <ngocquang12395@gmail.com>
2022-07-28 14:43:41 +0700

    ontap final

---
+++ src/ontap_final/service/IStudentService.java
@@ -0,0 +1,11 @@
+package ontap_final.service;
+
+import ss16_io_text.model.Student;
+
+import java.util.List;
+
+public interface IStudentService {
+    void add();
+
+    List<Student> findAll();
+}
